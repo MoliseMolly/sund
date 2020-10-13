a65f6116d76eba72d883187ef5fd3158a5ac5b2d
MoliseMolly <47737164+MoliseMolly@users.noreply.github.com>
2020-10-13 18:24:56 +0200

    parallax done

--- script.js
+++ script.js
@@ -41,7 +41,30 @@ gsap.to(".animation_top2", {
 
 });
 
-;
+/*innerHeight = elementets højde, dynamisk værdi*/
+/*her tages fat i alle parallax classerne*/
+const parallaxes = document.querySelectorAll(".parallax");
+/* først loopes igennem alle parallaxes,*/
+parallaxes.forEach((section, i) => {
+    /*Derefter kigges inde i sectionerne, og der kigges på bg classes*/
+    section.bg = section.querySelector(".bg");
+    /*der sættes en baggrund position, på 50% højsontalt, derefter sætter vi den verikale til at være halvdelel i elementets højde*/
+    section.bg.style.backgroundPosition = `50% ${innerHeight / 2}px`;
+    /*her sættes animatione igang, den styres af scroll eventet. */
+    gsap.to(section.bg, {
+        /*Animeres til følgende værdier/koordinater*/
+        /*Derefter sættes background position til 50% højsontalt og til minus halvdelen af elementets højde*/
+        backgroundPosition: `50% ${-innerHeight / 2}px`,
+        /**/
+        ease: "none",
+        scrollTrigger: {
+            trigger: section,
+            scrub: true,
+        },
+    });
+});
+
+/*
 (function ($, window, document, undefined) {
 
     var defaults = {
@@ -315,3 +338,4 @@ $(function () {
     }).trigger('resize');
 
 });
+*/
